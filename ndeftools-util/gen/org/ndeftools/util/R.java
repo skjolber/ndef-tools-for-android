de9655015fa4acc90d18882a8d57be007cfb3893
Thomas Skjølberg <thomas.skjolberg@greenbird.com>
2013-02-06 00:29:43 +0100

    project update

--- ndeftools-util/gen/org/ndeftools/util/R.java
+++ ndeftools-util/gen/org/ndeftools/util/R.java
@@ -10,13 +10,7 @@ package org.ndeftools.util;
 public final class R {
     public static final class attr {
     }
-    public static final class drawable {
-        public static int ic_launcher=0x7f020000;
-    }
     public static final class string {
-        public static int app_name=0x7f030000;
-    }
-    public static final class style {
-        public static int AppTheme=0x7f040000;
+        public static int app_name=0x7f020000;
     }
 }
